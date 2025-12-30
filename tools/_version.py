d1515a425e406634f3d66e0b713aae0a4e3f9d19
Daniel Glaser <me@the78mole.de>
2025-12-30 16:17:25 +0100

    Improve action workflow information

--- tools/_version.py
+++ tools/_version.py
@@ -27,7 +27,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = "0.0.2.post1.dev0+g306930dd2.d20251230"
-__version_tuple__ = version_tuple = (0, 0, 2, "post1", "dev0", "g306930dd2.d20251230")
+__version__ = version = "0.1.0"
+__version_tuple__ = version_tuple = (0, 1, 0)
 
 __commit_id__ = commit_id = None
